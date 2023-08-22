179ee631aa8ede0eedd0245207a444fc2e623e6c
Grace kungu <grayckungu2@gmail.com>
2023-08-22 19:33:56 +0300

    completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(arr):
+    if len(arr) == 0:
+        return ""
+    elif len(arr) == 1:
+        return arr[0]
+    elif len(arr) == 2:
+        return arr[0] + " and " + arr[1]
+    else:
+        return ", ".join(arr[:-1]) + ", and " + arr[-1]
+    
+    
+    
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi"]))  
+    
